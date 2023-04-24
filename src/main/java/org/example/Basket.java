6662d0a3400ec4e5cfda60bfcdacb47ecd1d9c0d
Viktoria Kravchenko <taiyibe@yandex.ru>
2023-04-24 06:57:11 +0300

    Первая версия программы

---
+++ src/main/java/org/example/Basket.java
@@ -0,0 +1,103 @@
+package org.example;
+
+import java.io.*;
+
+public class Basket {
+    private final int[] card;
+    private final int[] prices;
+    private final String[] products;
+
+    public Basket(String[] prods, int[] prc) {
+        products = prods;
+        prices = prc;
+        card = new int[prc.length];
+    }
+
+    public boolean addToCart(int productNum, int amount) {
+        if (productNum >= 0 && productNum < prices.length) {
+            card[productNum] += amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void printCart() {
+        int cardSumm = 0;
+        for (int j : card) {
+            cardSumm += j;
+        }
+        if (cardSumm > 0) {
+            int cardFinSumm = 0;
+            System.out.println("Ваша корзина: ");
+            for (int i = 0; i < card.length; i++) {
+                if (card[i] > 0) {
+                    cardFinSumm += card[i] * prices[i];
+                    System.out.printf("%s %d шт. %d руб/шт. %d рублей в сумме \n", products[i], card[i], prices[i], card[i] * prices[i]);
+                }
+            }
+            System.out.printf("Итог: %d руб. \n", cardFinSumm);
+        } else {
+            System.out.println("Ваша корзина пуста.");
+        }
+    }
+
+    public boolean saveTxt(File textFile) {
+        try (PrintWriter out = new PrintWriter(textFile)) {
+            for (String prd : products) {
+                out.print(prd + ' ');
+            }
+            out.print('\n');
+            for (int prc : prices) {
+                out.print(Integer.toString(prc) + ' ');
+            }
+            out.print('\n');
+            for (int cr : card) {
+                out.print(Integer.toString(cr) + ' ');
+            }
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static Basket loadFromTxtFile(File textFile) {
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(textFile))) {
+            String[] prd = fileReader.readLine().split(" ");
+            String[] prc_buf = fileReader.readLine().split(" ");
+            String[] cr_buf = fileReader.readLine().split(" ");
+
+            int[] prc = new int[prc_buf.length];
+            for (int i = 0; i < prc_buf.length; i++) {
+                prc[i] = Integer.parseInt(prc_buf[i]);
+            }
+
+            int[] cr = new int[cr_buf.length];
+            for (int i = 0; i < cr_buf.length; i++) {
+                cr[i] = Integer.parseInt(cr_buf[i]);
+            }
+
+            Basket rs = new Basket(prd, prc);
+
+            for (int i = 0; i < cr.length; i++) {
+                if (cr[i] > 0) {
+                    rs.addToCart(i, cr[i]);
+                }
+            }
+
+            return rs;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public int[] getPrices() {
+        return prices;
+    }
+
+    public String[] getProducts() {
+        return products;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Basket basket;
+        File bu = new File("basket.txt");
+        if (bu.exists() && !bu.isDirectory()) {
+            basket = Basket.loadFromTxtFile(bu);
+            if (basket == null) {
+                System.out.println("Ошибка загрузки корзины");
+                String[] products = {"Молоко", "Яйца", "Сахар", "Соль", "Мука", "Ванилин"};
+                int[] prices = {125, 85, 45, 25, 90, 5};
+                basket = new Basket(products, prices);
+            }
+        } else {
+            String[] products = {"Молоко", "Яйца", "Сахар", "Соль", "Мука", "Ванилин"};
+            int[] prices = {125, 85, 45, 25, 90, 5};
+            basket = new Basket(products, prices);
+        }
+
+        boolean isRunning = true;
+        Scanner scanner = new Scanner(System.in);
+        while (isRunning) {
+            for (int i = 0; i < basket.getPrices().length; i++) {
+                System.out.printf("[%d] %s - %d руб \n", i + 1, basket.getProducts()[i], basket.getPrices()[i]);
+            }
+            System.out.println("Введите номер продукта и его кол-во. \n Для завершения введите 'end'.");
+            String buff = scanner.nextLine();
+            String[] splBuff = buff.split("\\s+");
+            if (splBuff.length > 1) {
+                int prodNumber;
+                int amountProd;
+                try {
+                    prodNumber = Integer.parseInt(splBuff[0]) - 1;
+                    amountProd = Integer.parseInt(splBuff[1]);
+                } catch (NumberFormatException e) {
+                    System.out.println("Введен некоректный номер товара или его количество!\n");
+                    continue;
+                }
+                if (prodNumber < 0 || prodNumber >= basket.getProducts().length) {
+                    System.out.println("Введен некоректный номер товара!\n");
+                    continue;
+                }
+                if (amountProd < 1) {
+                    System.out.println("Введенно неверное количество товара!\n");
+                    continue;
+                }
+                if (basket.addToCart(prodNumber, amountProd)) {
+                    if (!basket.saveTxt(new File("basket.txt"))) {
+                        System.out.println("Ошибка сохранения");
+                    }
+                } else {
+                    System.out.println("Ошибка добавления");
+                }
+
+            } else {
+                if (splBuff[0].equals("end")) {
+                    isRunning = false;
+                }
+            }
+        }
+        basket.printCart();
+    }
+}
